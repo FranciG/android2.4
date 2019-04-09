86d991631480a7e1297ac1b989c59f81a9c40cef
FranciG <beodo0@gmail.com>
2019-04-09 13:41:59 +0300

    two working buttons so far

---
+++ app/src/main/java/com/example/lab24/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.lab24;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+Button btn1;
+EditText etexto;
+TextView texto;
+Button button2;
+    public void supBtn_clicked(){
+        String name = etexto.getText().toString();
+        texto.setText( "Hola " + name);
+    }
+    public void enBtn_clicked(){
+        String name = etexto.getText().toString();
+        texto.setText( "Hello " + name);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etexto = findViewById(R.id.editText1);
+       texto = findViewById(R.id.textView1);
+btn1 = findViewById(R.id.button1);
+button2 =findViewById(R.id.button2);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                supBtn_clicked();
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                enBtn_clicked();
+            }
+        });
+
+
+    }
+}
